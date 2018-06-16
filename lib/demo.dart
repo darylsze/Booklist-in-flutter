9c294f5f195f4f1789f38a338f9b5984a9fb65be
Daryl <sze.kf1004@gmail.com>
2018-06-16 17:19:20 +0800

    chore: project init

---
+++ lib/demo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'welcome to demo.dart',
+      home: new Scaffold(),
+    )
+  }
+}
