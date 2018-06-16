37ea3c6d9410b16606a56bf2a3f173b32d1ee137
Daryl <sze.kf1004@gmail.com>
2018-06-16 19:28:49 +0800

    fixup! feat: add dialog for page selection

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,16 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+          // counter didn't reset back to zero; the application is not restarted.
+//        primarySwatch: Colors.,
+          primaryColor: Colors.white
       ),
       home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -119,14 +120,14 @@ class _MyHomePageState extends State<MyHomePage> {
         barrierDismissible: true,
         child: new SimpleDialog(
           children: <Widget>[
-            new MaterialButton(
+            new SimpleDialogOption(
               child: new Text('Show simple list'),
               onPressed: () {
                 print('pressed simple list');
                 _showSimpleList();
               },
             ),
-            new MaterialButton(
+            new SimpleDialogOption(
                 child: new Text('Show dynamic item list'),
                 onPressed: () {
                   print('pressed dynamic item list');
